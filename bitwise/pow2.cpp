9908b174f04bf8fd472672bd3b9628a9d27c918b
Fardin2112 <fardinalam2112@gmail.com>
2025-01-30 12:19:01 +0530

    update

---
+++ bitwise/pow2.cpp
@@ -0,0 +1,28 @@
+// figure out how to find if a number is power of 2 without any loop
+
+#include<iostream>
+using namespace std;
+
+int decToBin(int num){
+
+    int ans = 0, pow = 1;
+
+    while(num != 0){
+        int rem = num % 2;
+        num = num / 10 ;
+    }
+}
+
+bool pow2(int num){
+
+    int binNum = 
+}
+
+
+int main() {
+
+    int num = 10;
+    powOf2(num);
+
+    return 0;
+}
